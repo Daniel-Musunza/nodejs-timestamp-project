4396e5c1370b2bc6f1f091c1dd3882c0fd113890
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-07-14 10:11:33 +0300

    commits

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { DateTime } = require('luxon');
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/:date', function(req, res) {
+app.get('/api/:date?', function (req, res) {
   const dateParam = req.params.date;
-  let date;
+  if(dateParam){
+    let date;
 
-  if (/\D/.test(dateParam)) {
-    date = new Date(dateParam);
-  } else {
-    date = new Date(parseInt(dateParam));
-  }
+    if (/^\d{5,}$/.test(dateParam)) {
+      date = new Date(parseInt(dateParam));
+    } else {
+      date = new Date(dateParam);
+    }
 
-  if (date.toString() === 'Invalid Date') {
-    res.json({ error: 'Invalid Date' });
+    if (date.toString() === 'Invalid Date') {
+      res.json({ error: 'Invalid Date' });
+    } else {
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+      });
+    }
   } else {
+    const currentTime = DateTime.now().setZone('Africa/Nairobi').toMillis();
+    const currentUTC = DateTime.now().toUTC().toRFC2822();
     res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
+      unix: currentTime,
+      utc: currentUTC
     });
   }
+ 
 });
 
-const listener = app.listen(process.env.PORT || 3000, function() {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
